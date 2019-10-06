1f6ca6b1323aff788c6263d0ef0619d05b0df60c
HexadecimalHUN <34522741+HexadecimalHUN@users.noreply.github.com>
2019-10-06 23:51:24 +0200

    Update euler15.py

--- euler15.py
+++ euler15.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2017, Bodnar Peter, sajtos0007@gmail.com
+# Copyright (C) 2019, Bodnar Peter, sajtos0007@gmail.com
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
